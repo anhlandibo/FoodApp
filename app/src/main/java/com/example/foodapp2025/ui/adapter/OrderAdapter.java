b290ccb26edd2df39b8bddb160a3bb1b2b0dc298
Phu Tho <phanphutho9999@gmail.com>
2025-04-14 14:12:54 +0700

    OrderAdapter psuedo-completed, not yet understand nor sure it will works, will need attention later to set listener. working on repo and remote.

--- app/src/main/java/com/example/foodapp2025/ui/adapter/OrderAdapter.java
+++ app/src/main/java/com/example/foodapp2025/ui/adapter/OrderAdapter.java
@@ -16,69 +16,61 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.foodapp2025.R;
 import com.example.foodapp2025.data.model.FoodModel;
+import com.example.foodapp2025.data.model.OrderModel;
 
 import java.util.ArrayList;
 
-public class OrderAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder> {
-    private ArrayList<FoodModel> foodModels = new ArrayList<>();
+public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHolder> {
+    private ArrayList<OrderModel> orderModels = new ArrayList<>();
     public OrderAdapter(){}
-    public OrderAdapter(ArrayList<FoodModel> foodModels){
-        this.foodModels = foodModels;
+    public OrderAdapter(ArrayList<OrderModel> orderModels){
+        this.orderModels = orderModels;
     }
     @SuppressLint("NotifyDataSetChanged")
-    public void setFoodList(ArrayList<FoodModel> foodModels) {
-        this.foodModels = foodModels;
+    public void setOrderList(ArrayList<OrderModel> orderModels) {
+        this.orderModels = orderModels;
         notifyDataSetChanged();
     }
 
     @NonNull
     @Override
-    public FoodAdapter.FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_food, parent, false);
-        return new FoodAdapter.FoodViewHolder(view);
+    public OrderAdapter.OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.order_item, parent, false);
+        return new OrderAdapter.OrderViewHolder(view);
     }
 
     @SuppressLint("SetTextI18n")
     @Override
-    public void onBindViewHolder(@NonNull FoodAdapter.FoodViewHolder holder, int position) {
-        FoodModel foodModel = foodModels.get(position);
-        holder.foodName.setText(foodModel.getName());
-        holder.foodPrice.setText(foodModel.getPrice() + " VND");
-        holder.foodStart.setText(String.valueOf(foodModel.getStar()));
-        holder.foodTime.setText(foodModel.getTime());
-
-        Glide.with(holder.itemView.getContext())
-                .load(foodModel.getImageUrl())
-                .into(holder.foodImage);
+    public void onBindViewHolder(@NonNull OrderAdapter.OrderViewHolder holder, int position) {
+        OrderModel orderModel = orderModels.get(position);
+        holder.orderId.setText(orderModel.getId());
+        holder.orderTime.setText(String.valueOf(orderModel.getTimestamp()));
+        holder.orderStatus.setText(String.valueOf(orderModel.getStatus()));
 
 
-        holder.itemView.setOnClickListener(v -> {
-            FoodModel selectedFood = foodModels.get(position);
-            NavController navController = Navigation.findNavController(v);
-            Bundle bundle = new Bundle();
-            bundle.putString("foodName", foodModel.getName());
-            bundle.putSerializable("food", selectedFood);
-            navController.navigate(R.id.foodDetailFragment, bundle);
-        });
+//        holder.itemView.setOnClickListener(v -> {
+//            FoodModel selectedFood = foodModels.get(position);
+//            NavController navController = Navigation.findNavController(v);
+//            Bundle bundle = new Bundle();
+//            bundle.putString("foodName", foodModel.getName());
+//            bundle.putSerializable("food", selectedFood);
+//            navController.navigate(R.id.foodDetailFragment, bundle);
+//        });
     }
 
     @Override
     public int getItemCount() {
-        return foodModels.size();
+        return orderModels.size();
     }
 
-    public static class FoodViewHolder extends RecyclerView.ViewHolder {
-        ImageView foodImage;
-        TextView foodName, foodPrice, foodTime, foodStart;
+    public static class OrderViewHolder extends RecyclerView.ViewHolder {
+        TextView orderId, orderStatus, orderTime;
 
-
-        public FoodViewHolder(@NonNull View itemView) {
+        public OrderViewHolder(@NonNull View itemView) {
             super(itemView);
-            foodImage = itemView.findViewById(R.id.foodImage);
-            foodName = itemView.findViewById(R.id.foodName);
-            foodPrice = itemView.findViewById(R.id.foodPrice);
-            foodTime = itemView.findViewById(R.id.popularFoodTime);
-            foodStart = itemView.findViewById(R.id.ratingTxt);
+            orderId = itemView.findViewById(R.id.orderId);
+            orderStatus = itemView.findViewById(R.id.orderStatus);
+            orderTime = itemView.findViewById(R.id.orderTime);
         }
     }
 }
