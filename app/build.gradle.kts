d53fb8efccc2e63b1c31a84fdf37961a9cb551c3
anhlandibo <ngominhtri9107@gmail.com>
2025-04-20 16:18:51 +0700

    chore: setting up for cloud messaging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     // Firebase libraries
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.messaging)
 
     // Google Sign-In libraries
     implementation(libs.credentials)
@@ -67,6 +68,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0") // Avoid duplicating Glide versions
     implementation(libs.firebase.storage)
     implementation(libs.play.services.maps)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
